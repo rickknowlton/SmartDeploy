16d8e1a174e5f7617c5f218c1167cfae16c6b982
rickknowlton <richard.d.knowlton@gmail.com>
2023-04-08 16:07:13 -0400

    v 1.3.0 --feat: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,52 +1,64 @@
-import '../assets/js/app-head.js';
-import '../assets/js/uikit-components.js';
-import '../assets/js/data-attr-helper.js';
-import '../assets/js/anime-helper.js';
-import '../assets/js/swiper-helper.js';
-import '../assets/js/typed-helper.js';
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
-import React, { useEffect } from 'react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import  '../assets/css/uikit.min.css'; 
-import '../assets/css/theme/main.css';
+import "../assets/js/app-head.js";
+import "../assets/js/uikit-components.js";
+import "../assets/js/data-attr-helper.js";
+import "../assets/js/anime-helper.js";
+import "../assets/js/swiper-helper.js";
+import "../assets/js/typed-helper.js";
+import * as gtag from "../lib/gtag";
 
-function MyApp({ Component, pageProps }) {
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "../assets/css/uikit.min.css";
+import "../assets/css/theme/main.css";
 
+function App({ Component, pageProps }) {
   const [loading, setLoading] = React.useState(false);
   const [firstRender, setFirstRender] = React.useState(false);
 
-  useEffect(() => {  
-	   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            const target = document.querySelector(this.getAttribute('href'));
-            if (!target) return;
-            e.preventDefault();    
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-    
-    
-  })
+  const router = useRouter();
 
-  React.useEffect(() =>{    
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+        const target = document.querySelector(this.getAttribute("href"));
+        if (!target) return;
+        e.preventDefault();
+        target.scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+  });
+
+  React.useEffect(() => {
     setFirstRender(true);
-  })
+  });
 
   if (!firstRender) {
     return <></>;
   }
   return (
     <>
-   
       <React.Fragment>
-          <Component {...pageProps} />
-        </React.Fragment>
+        <Component {...pageProps} />
+      </React.Fragment>
     </>
-  )
+  );
 }
-export default MyApp;
+export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default function Document() {
   return (
@@ -7,6 +8,23 @@ export default function Document() {
       className="uk-background-white dark:uk-background-gray-100 dark:uk-text-gray-20"
     >
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+          }}
+        />
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
