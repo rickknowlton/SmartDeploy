3cba655ecf561d7bc450a4d1503a7364d57df09b
rickknowlton <richard.d.knowlton@gmail.com>
2023-04-11 13:25:02 -0400

    v 1.5.0 --feat: added metatags

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,37 @@ export default function Document() {
       className="uk-background-white dark:uk-background-gray-100 dark:uk-text-gray-20"
     >
       <Head>
+        {/* Basic meta tags for SEO */}
+        <title>SmartDeploy || Ready. Set SmartDeploy.</title>
+        <meta name="description" content="Easily manage smart contracts on Soroban with our all-in-one platform for publishing, deploying, invoking, and upgrading contracts." />
+        <meta name="keywords" content="smartdeploy, smart deploy, TENK, TENK DAO, stellar, stellar soroban, stellar xlm, xlm, smart contract management, soroban, smart contract deployment, user-friendly, secure platform, contract deployment, developers, tools, smart contracts, contract management, blockchain technology, DAO, decentralized finance, ethereum, cryptocurrency, NFT, tokenization, solidity, Ethereum development, smart contract security, blockchain solutions, DeFi solutions, decentralized applications, Web3, crypto assets, digital tokens, governance tokens" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="SmartDeploy || Ready. Set SmartDeploy." />
+        <meta
+          property="og:description"
+          content="Easily manage smart contracts on Soroban with our all-in-one platform for publishing, deploying, invoking, and upgrading contracts."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://smartdeploy.dev" />
+        <meta
+          property="og:image"
+          content="../public/images/demo.png"
+        />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="SmartDeploy || Ready. Set SmartDeploy." />
+        <meta
+          name="twitter:description"
+          content="Easily manage smart contracts on Soroban with our all-in-one platform for publishing, deploying, invoking, and upgrading contracts."
+        />
+        <meta
+          name="twitter:image"
+          content="../public/images/demo.png"
+        />
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
           async
@@ -25,7 +56,7 @@ export default function Document() {
               `,
           }}
         />
-        {/* CookieYes Banner */}        
+        {/* CookieYes Banner */}
         <script
           id="cookieyes"
           type="text/javascript"
