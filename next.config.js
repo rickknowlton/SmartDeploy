6b20c88d9bc2e0e7866c9031de7d8f3460f7e523
rickknowlton <richard.d.knowlton@gmail.com>
2023-04-19 18:10:01 -0400

    v 1.6.6 --tweak: removed redirect from next.config and added vercel.json to handle redirects

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+}
 
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.smartdeploy.dev',
-          },
-        ],
-        destination: 'https://smartdeploy.dev/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
 
-module.exports = nextConfig;
+module.exports = nextConfig
