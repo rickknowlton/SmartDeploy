9c1d3a2281ac4dab019c78c37570872ec681fd82
rickknowlton <richard.d.knowlton@gmail.com>
2023-04-19 18:26:17 -0400

    v 1.6.8 --bugfix: rollback of vercel.json -> next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
