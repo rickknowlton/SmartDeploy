26a4edd3c11d9ce4fc3016a758cca8b5d4875080
rickknowlton <richard.d.knowlton@gmail.com>
2023-04-19 18:06:31 -0400

    v 1.6.4 --minor: added redirect to canonical in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.smartdeploy.dev',
+          },
+        ],
+        destination: 'https://smartdeploy.dev/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
